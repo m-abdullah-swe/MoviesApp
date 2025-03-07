fb788856365243721883f182abb846532adf7420
m-abdullah-swe <m.abdullah.swe@gmail.com>
2025-03-07 17:20:36 +0500

    Refactored Code.

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import MovieSearchScreen from "./screens/MovieSearchScreen";
 import MovieDetailsScreen from "./screens/MovieDetailsScreen";
 
 const Stack = createStackNavigator();
-// heheeh
+
+
 const App = () => {
   return (
     <NavigationContainer>
